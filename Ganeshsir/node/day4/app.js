0a56451bd2a6955f8ac887aa2ea372f3cd6851b0
BabulHoda98 <babulhoda98@gmail.com>
2025-05-12 16:26:24 +0530

    updated

---
+++ Ganeshsir/node/day4/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+let PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("Request received: ", req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    `<h1>Hello, World!</h1><p>This is a simple Node.js application.</p> <p>hello</p> 
+     <h1>hello</h1>
+     <p>hello</p>
+     <p>NPM</p>
+     <p>NPM init</p>
+     <p>NPM init -y</p>
+     <p>NPM i nodemon</p>
+     <p>NPM  i nodemon</p>
+    `
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port at http://localhost:${PORT}`);
+});
