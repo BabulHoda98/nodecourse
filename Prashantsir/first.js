42272e5d4e278c2c0d7669e72261606a8181f5ea
BabulHoda98 <babulhoda98@gmail.com>
2025-05-21 09:39:40 +0530

    lecture3

--- Prashantsir/first.js
+++ Prashantsir/first.js
@@ -1,6 +1,6 @@
 console.log("This is Node course");
 
-const fs = require("fs");
+const fs = require('fs');
 fs.writeFileSync("output.txt", "Writting File",(err)=>{
     if(err)console.log("Error occured");
         else console.log("File written Successfully");
